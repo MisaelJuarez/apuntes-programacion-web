aa23bf839685b90299d12a0e3f3e50437f9838ce
Misael Juarez <138533025+MisaelJuarez@users.noreply.github.com>
2023-12-03 21:29:50 -0600

    Modificaciones

--- Programa/controllers/contacto.js
+++ Programa/controllers/contacto.js
@@ -42,3 +42,5 @@ module.exports = {
     actualizarContacto,
     eliminarContacto
 }
+
+ 
--- Programa/database/config.js
+++ Programa/database/config.js
@@ -1,12 +1,17 @@
-const { default: mongoose } = require("mongoose"); // Importaciones en ES5
+const { default: mongoose } = require("mongoose"); // Importa el módulo mongoose.
 
-const conexion = async() => {
+const conexion = async() => { // Define una función llamada conexion.
+    // Intenta conectarse a la base de datos MongoDB Atlas.
     try {
+        // Parámetros de la función connect()
         await mongoose.connect("mongodb+srv://Misael:MisaelZKR22@cluster0.6izxmca.mongodb.net/agenda");
+        // Imprime un mensaje de éxito si la conexión se realizó correctamente.
         console.log("Se conecto al mongoDB Atlas");
     } catch (error) {
+        // Lanza un error si la conexión no se pudo realizar
         throw new Error("Error al conectarse al mongoDB Atlas");
     }
 }
-//esportaciones en ES5
+
+// Exporta la función conexion
 module.exports = conexion;
